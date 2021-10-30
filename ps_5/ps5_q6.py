61008aab5f4f8032fcc3657679c0caf0a4d88000
Minya Bai <minyabai25@gmail.com>
2021-10-30 13:47:15 -0400

    fixed issues and added comments

--- ps_5/ps5_q6.py
+++ ps_5/ps5_q6.py
@@ -1,10 +1,25 @@
+## PHYS512 - Problem Set 5
+## ===========================
+## Minya Bai (260856843)
+## Question 6
+
 import numpy as np
 from matplotlib import pyplot as plt
+from functions import corr
 
-n = 5
-N = 100
-randwalk = np.cumsum(np.random.randn(n))
-dx = np.)
-k = np.arange(1,15)
+n = 500
+randwalk = np.cumsum(np.random.randn(n)) # gen rand walk
+
+plt.plot(randwalk)
+plt.show()
+
+N = np.arange(n, dtype='float')[1:]**-2 # we expect to scale by k**-2
+f2 = corr(randwalk,randwalk) # calculate correlation function
+fft_f2 = np.fft.fft(f2) # ft to get power spectrum
+
+
+plt.plot(np.fft.fft(f2))
+plt.plot(np.max(fft_f2)*N)
+plt.show()
 
 
