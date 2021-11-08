de3237645025aa7eeac1189f09b7235f4c11481e
Minya Bai <minyabai25@gmail.com>
2021-11-08 17:44:09 -0500

    updated functions

--- ps_6/ps_6.py
+++ ps_6/ps_6.py
@@ -7,40 +7,43 @@ import json
 from matplotlib import pyplot as plt
 from simple_read_ligo import *
 import scipy.signal as sig
-        
+from scipy.interpolate import interp1d
+
 def smooth(data):
     k = np.arange(len(data))
     g = np.fft.fft(np.exp(-k**2))
     h = np.abs(np.fft.ifft(np.fft.fft(data)*g))
     return h
 
-def whiten(data, noise, freq, template, dt):
-    fft_dat1 = np.fft.fft(data*sig.get_window(window='tukey', Nx=len(data)))
-    fft_dat2 = np.fft.fft(template*sig.get_window(window='tukey', Nx=len(template)))
-    fft_freq = np.fft.fftfreq(len(template),dt)
+def whiten(strain, noise, freq, dt):
+    Nt = len(strain)
+    fft_freq = np.fft.rfftfreq(Nt, dt)
+    norm_f = np.sqrt((fft_freq[1] - fft_freq[0])/2048)
     noise_dat = np.interp(np.abs(fft_freq),freq,noise)
-    spectr1 = fft_dat1/np.sqrt(noise_dat)
-    spectr2 = fft_dat2/np.sqrt(noise_dat)
-    white1 = np.fft.ifft(spectr1)
-    white2 = np.fft.ifft(spectr2)
-
-    return white1, white2, freq
+    
+    fft_dat = np.fft.rfft(strain*sig.get_window(window='tukey', Nx = Nt))
+    norm = 1./np.sqrt(1./(dt**2))
+    white_dat = fft_dat/np.sqrt(noise_dat) * norm
+    white_dat = np.fft.irfft(white_dat,Nt) / norm_f
+    
+    return white_dat
 
 def match_filter(data, noise, freq, template, dt):
-    fft_dat = np.fft.fft(data) * dt 
-    fft_spectr = np.fft.fft(template) * dt
+    fft_dat = np.fft.fft(data*sig.tukey(data.size,alpha=1./8)) * dt 
+    fft_template = np.fft.fft(template*sig.tukey(template.size,alpha=1.8)) * dt
     fft_freq = np.fft.fftfreq(len(template)) * dt
+    power_vec = np.interp(np.abs(fft_freq),freq,noise)
         
-    MF = np.conjugate(fft_spectr) * fft_dat
-    MF = np.real(np.fft.ifft(MF))
+    MF = fft_dat * fft_template.conjugate() / power_vec
+    MF = np.fft.irfft(MF,len(data))
 
     scale = np.abs(MF).sum() * dt
 
-    t = np.arange(len(data) * dt, step=dt)
+    t = 2 * np.arange(len(data) * dt, step=dt) / dt
 
     return MF/scale, t
 
-def snr(data, noise, freq, template, dt):
+def snr(strain, noise, freq, template, dt):
     fft_freq = np.fft.fftfreq(len(template)) / dt
     df = fft_freq[1] - fft_freq[0]
     noise_dat = np.interp(np.abs(fft_freq), fft_freq, data)
@@ -100,7 +103,7 @@ for i in range(len(H1)):
     noise_L.append(smooth(noise_spec_L))
     freq_H.append(freqs_H1)
     freq_L.append(freqs_L1)
-
+    
 plt.loglog(noise_H[-1])
 plt.loglog(noise_L[-1])
 plt.show()
@@ -114,10 +117,12 @@ Ltshift = []
 
 # whiten_data = whiten(H1[0][0],noise_H[0])
 for i in range(4):
-    w_H,w_Ht,w_Hfreq = whiten(H1[i][0],noise_H[i],freq_H[i],template_H[i],H1[i][1])
-    w_L,w_Lt,w_Lfreq = whiten(L1[i][0],noise_L[i],freq_L[i],template_L[i],L1[i][1])
-    mf_H,Ht = match_filter(w_H,noise_H[i],w_Hfreq,w_Ht,H1[i][1])
-    mf_L,Lt = match_filter(w_L,noise_L[i],w_Lfreq,w_Lt,L1[i][1])
+    w_H = whiten(H1[i][0],noise_H[i],freq_H[i],H1[i][1])
+    w_Ht = whiten(template_H[i],noise_H[i],freq_H[i],H1[i][1])
+    w_L = whiten(L1[i][0],noise_L[i],freq_L[i],L1[i][1])
+    w_Lt = whiten(template_L[i],noise_L[i],freq_L[i],L1[i][1])
+    mf_H,Ht = match_filter(w_H,noise_H[i],freq_H[i],w_Ht,H1[i][1])
+    mf_L,Lt = match_filter(w_L,noise_L[i],freq_L[i],w_Lt,L1[i][1])
     mfH.append(mf_H)
     mfL.append(mf_L)
     Ht_shift = np.fft.fftshift(Ht)
@@ -126,6 +131,7 @@ for i in range(4):
     plt.plot(Lt_shift,mf_L,'blue',alpha=0.6)
     plt.show()
 
+'''
 ## Part c)
 for i in range(4):
     w_H,w_Ht,w_Hfreq = whiten(H1[i][0],noise_H[i],freq_H[i],template_H[i],H1[i][1])
@@ -135,3 +141,4 @@ for i in range(4):
     plt.plot(snr_H)
     plt.plot(snr_L)
     plt.show()
+'''
