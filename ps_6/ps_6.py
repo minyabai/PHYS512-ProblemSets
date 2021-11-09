151365b2c07b9c31c29a4e8870031f9afb4a89a1
Minya Bai <minyabai25@gmail.com>
2021-11-08 23:54:22 -0500

    completed up to part f

--- ps_6/ps_6.py
+++ ps_6/ps_6.py
@@ -9,6 +9,9 @@ from simple_read_ligo import *
 import scipy.signal as sig
 from scipy.interpolate import interp1d as interp1d
 
+## Functions
+## ==========================
+
 def smooth(data):
     k = np.arange(len(data))
     g = np.fft.fft(np.exp(-0.5*k**2))
@@ -22,13 +25,6 @@ def whiten(strain, noise, freq, dt):
     fft_freq = np.fft.fftfreq(Nt, dt)
     dat = fft_dat/np.sqrt(noise_interp(np.abs(fft_freq)))
     white_dat = np.fft.irfft(dat,Nt)
-    # norm_f = np.sqrt((fft_freq[1] - fft_freq[0])/2048)
-    # noise_dat = np.interp(np.abs(fft_freq),freq,noise)
-    
-    # fft_dat = np.fft.fft(strain*sig.get_window(window='tukey',Nx=Nt))
-    # norm = 1./np.sqrt(1./(dt**2))
-    # white_dat = fft_dat/np.sqrt(noise_dat) * norm
-    # white_dat = np.fft.irfft(white_dat,Nt) / norm_f
     
     return white_dat, fft_freq
 
@@ -36,10 +32,8 @@ def match_filter(w_data, noise, freq, template, dt):
     fs = 1/dt
     fft_dat = np.fft.fft(w_data) / fs 
     fft_template = np.fft.fft(template) / fs
-    # fft_freq = np.fft.fftfreq(len(template)) / fs
-    # power_vec = np.interp(np.abs(fft_freq),freq,noise)
         
-    MF = fft_dat * fft_template.conjugate() # / power_vec
+    MF = fft_dat * fft_template.conjugate()
     MF = np.fft.irfft(MF,len(w_data))
 
     t = 2 * np.arange(len(w_data) * dt, step=dt) / dt
@@ -65,6 +59,13 @@ def snr(w_strain, template, w_template, freq, dt):
     SNR = np.abs(np.roll(SNR,shift))
 
     return SNR
+
+def mid_freq(data, freq):
+    pdf = np.cumsum(np.abs(data))
+    return freq[(pdf < max(pdf)/2)][-1]
+
+## Reading Data
+## ===========================
 
 data_loc = 'LOSC_Event_tutorial/'
 
@@ -92,7 +93,7 @@ for i in range(N_events):
 
 file.close()
 
-##############################
+## Initialize Arrays
 
 plot = False
 
@@ -101,15 +102,10 @@ noise_L = []
 freq_H = []
 freq_L = []
 
-# noise_H = get_noise(H1)
-# noise_L = get_noise(L1)
-
 ## Part a)
 for i in range(len(H1)):
     H_strain, H_dt = H1[i][:2]
     L_strain, L_dt = L1[i][:2]
-    # noise_spec_H = get_noise(H_strain)
-    # noise_spec_L = get_noise(L_strain)
     freqs_H1, noise_spec_H = sig.welch(H_strain,fs=1/H_dt,nperseg=1/H_dt,window='tukey')
     freqs_L1, noise_spec_L = sig.welch(L_strain,fs=1/L_dt,nperseg=1/L_dt,window='tukey')
     # print(len(noise_spec_H))
@@ -124,10 +120,8 @@ for i in range(len(H1)):
         plt.show()
 
 ## Part b)
-wH = [] # 0 - strain, 1 - template
+wH = [] # 0 - strain, 1 - strain freq, 2 - template, 3 - template freq
 wL = []
-fH = []
-fL = []
 
 mfH = []
 mfL = []
@@ -141,10 +135,8 @@ for i in range(4):
     w_Ht,f_Ht = whiten(template_H[i],noise_H[i],freq_H[i],H1[i][1])
     w_L,f_L = whiten(L1[i][0],noise_L[i],freq_L[i],L1[i][1])
     w_Lt,f_Lt = whiten(template_L[i],noise_L[i],freq_L[i],L1[i][1])
-    wH.append([w_H,w_Ht])
-    wL.append([w_L,w_Lt])
-    fH.append([f_H,f_Ht])
-    fL.append([f_L,f_Lt])
+    wH.append([w_H,f_H,w_Ht,f_Ht])
+    wL.append([w_L,f_L,w_Lt,f_Lt])
     mf_H,Ht = match_filter(w_H,noise_H[i],f_Ht,w_Ht,H1[i][1])
     mf_L,Lt = match_filter(w_L,noise_L[i],f_Lt,w_Lt,L1[i][1])
     mfH.append(mf_H)
@@ -164,14 +156,13 @@ for i in range(4):
     H_snr = np.max(np.abs(mfH[i])/H_noise)
     L_noise = np.std(mfL[i][10000:40000])
     L_snr = np.max(np.abs(mfL[i])/L_noise)
-    print("Hanford: {}, Livingston: {}".format(H_snr,L_snr))
-
+    HL_snr = np.sqrt(H_snr**2 + L_snr**2)
+    print("Hanford: {}, Livingston: {}, Combined: {}".format(H_snr,L_snr,HL_snr))
+    
 ## Part d)
 for i in range(4):
-    w_H,w_Ht = wH[i]
-    w_L,w_Lt = wL[i]
-    f_H,f_Ht = fH[i]
-    f_L,f_Lt = fL[i]
+    w_H,f_H,w_Ht,f_Ht = wH[i]
+    w_L,f_L,w_Lt,f_Lt = wL[i]
 
     H_SNR = snr(w_H,template_H[i],w_Ht,f_Ht,H1[i][1])
     L_SNR = snr(w_H,template_L[i],w_Lt,f_Lt,L1[i][1])
@@ -179,8 +170,41 @@ for i in range(4):
     maxH_SNR = H_SNR[np.argmax(H_SNR)]
     maxL_SNR = L_SNR[np.argmax(L_SNR)]
 
-    print("Hanford: {}, Livingston: {}".format(maxH_SNR,maxL_SNR))
+    HL_SNR = np.sqrt(maxH_SNR**2 + maxL_SNR**2)
+
+    print("Hanford: {}, Livingston: {}, Combined: {}".format(maxH_SNR,maxL_SNR,HL_SNR))
+
+    if plot:
+        plt.plot(H_SNR)
+        plt.plot(L_SNR)
+        plt.show()
+
+## Part e)
+freqs = []
+
+for i in range(4):
+    w_H,f_H,w_Ht,f_Ht = wH[i]
+    w_L,f_L,w_Lt,f_Lt = wL[i]
+    Hfreq = np.fft.rfftfreq(len(w_Ht),H1[i][1])
+    Lfreq = np.fft.rfftfreq(len(w_Lt),L1[i][1])
+    Htemp = np.fft.rfft(w_Ht)
+    Ltemp = np.fft.rfft(w_Lt)
+    f_H = mid_freq(Htemp,Hfreq)
+    f_L = mid_freq(Ltemp,Lfreq)
+    freqs.append([f_H,f_L])
+    print("Hanford: {}, Livingston: {}".format(f_H,f_L))
+
+## Part f)
+dt = []
+for i in range(4):
+    Ht = Htshift[i][np.argmax(mfH[i])]
+    Lt = Ltshift[i][np.argmax(mfL[i])]
+    dt.append(np.abs(Ht-Lt))
+
+del_t = np.mean(dt)
+del_p = del_t * (3*10**8)
+
+print("Uncertainty in time: {}s".format(del_t))
+print("Uncertainty in position: {}s".format(del_p))
     
-    plt.plot(H_SNR)
-    plt.plot(L_SNR)
-    plt.show()
+    
