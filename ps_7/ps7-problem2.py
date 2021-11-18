8a92d4093eff1f68fd15c3079a1763909dea6642
Minya Bai <minyabai25@gmail.com>
2021-11-18 12:13:10 -0500

    Code for problem 2

---
+++ ps_7/ps7-problem2.py
@@ -0,0 +1,75 @@
+## PHYS512 - Problem Set 7
+## ============================
+## Minya Bai (260856843)
+## Question 2
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+def gauss(x,sigma):
+    return np.exp(-x**2/(2*sigma**2))
+
+def lorentz(x):
+    return 1/(1+x**2)
+
+def power(x,a,b,c):
+    return (x+a)**b + c
+
+## Taking a look at the exp in comparision to the possible options
+
+# x = np.linspace(0,5,1000)
+
+# plt.plot(x, gauss(x,0.5), label = 'Gaussian')
+# plt.plot(x, lorentz(x), label = 'Lorentzian')
+# plt.plot(x, power(x,0.5,-0.5,0), label = 'Power')
+# plt.plot(x, np.exp(-x), label = 'Exponential')
+# plt.legend()
+# plt.show()
+
+## Lorentzian is best option for the probability density function
+## since all the points are above the exponential
+## Let y=int lorentz = atan(x), so x = tan(y) for y \in (-\pi/2,\pi/2)
+## So P(y) = P(tan(y))
+
+n = 1000000
+y = np.pi*(np.random.randn(n)-0.5)
+dev = np.tan(y)
+dev_bound = dev[np.abs(dev)<25]
+
+a,bins = np.histogram(dev_bound, 100)
+binC = (bins[1:]+bins[:-1])/2
+
+pred = lorentz(binC)
+pred = pred/pred.sum()
+a = a/a.sum()
+
+## Plot comparison between lorentz and deviate
+# plt.plot(binC, a/a.max(), label='histogram')
+# plt.plot(binC, pred/pred.max(), label='lorentzian')
+# plt.legend()
+# plt.show()
+
+# we only want non-negative exp dev
+dev_cut = dev[dev > 0]
+p = 1.0
+acc_prob = p * np.exp(-dev_cut)/lorentz(dev_cut)
+acc = np.random.rand(len(acc_prob)) < acc_prob
+CDF = dev_cut[acc]
+
+print("Max Accepted fraction: ", np.max(acc_prob))
+print("Accepted fraction: ", len(CDF)/len(dev_cut))
+
+CDF_np = np.random.exponential(1,len(CDF))
+
+CDF.sort()
+CDF_np.sort()
+
+## Plotting CDF
+# plt.plot(CDF)
+# plt.plot(CDF_np,'--')
+# plt.show()
+
+## Plotting Histogram
+# plt.hist(CDF, bins=200, density=True)
+# plt.hist(CDF_np, bins=200, density=True)
+# plt.show()
