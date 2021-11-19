2e10efd0b7ad3e4411786dd3579e11071694ec34
Minya Bai <minyabai25@gmail.com>
2021-11-18 22:02:06 -0500

    Update graphs for np number generator

--- ps_7/ps7-problem1.py
+++ ps_7/ps7-problem1.py
@@ -9,13 +9,17 @@ from matplotlib import pyplot as plt
 x1,y1,z1 = np.loadtxt("rand_points.txt").T
 x2,y2,z2 = np.loadtxt("rand_points_np.txt").T
 
-fig = plt.figure()
-ax = plt.axes(projection='3d')
+a = 5.5
+b = -3
 
-ax.scatter(x1,y1,z1,'.',s=0.1)
+fig, ax = plt.subplots(1,2)
+
+ax[0].set_title('Using C rand int generator')
+ax[0].plot(a*x1+b*y1, z1, '.', markersize=0.5)
+
+ax[1].set_title('Using np rand int generator')
+ax[1].plot(a*x2+b*y2, z2, '.', markersize=0.5)
 plt.show()
 
-ax.clear()
-ax.scatter(x2,y2,z2,'.',s=0.1)
-plt.show()
+
 
